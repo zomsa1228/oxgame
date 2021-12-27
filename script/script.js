1787a0ad07e66837dbfdb55d9632103ddc3f16ac
Roimy_Zomsa <Adventure1611@gmail.com>
2021-12-27 14:34:25 +0900

    1.1update

--- script/script.js
+++ script/script.js
@@ -1,9 +1,67 @@
-function change()
+function change1()
     {
     var elem= document.getElementById("myButton1");
       if (elem.value=="✕") elem.value= "〇";
       else elem.value= "✕";
     }
+
+    function change2()
+    {
+    var elem= document.getElementById("myButton2");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+
+    function change3()
+    {
+    var elem= document.getElementById("myButton3");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+
+    function change4()
+    {
+    var elem= document.getElementById("myButton4");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+
+    function change5()
+    {
+    var elem= document.getElementById("myButton5");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+
+    function change6()
+    {
+    var elem= document.getElementById("myButton6");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+
+    function change7()
+    {
+    var elem= document.getElementById("myButton7");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+
+    function change8()
+    {
+    var elem= document.getElementById("myButton8");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+
+    function change9()
+    {
+    var elem= document.getElementById("myButton9");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+
+    
  function sample(){
    document.getElementById("area1").innerText = "変更しました";
  }
