e82f35052dcafd7c83074735078a334f7ce9160a
Roimy_Zomsa <Adventure1611@gmail.com>
2021-12-27 14:17:29 +0900

    create

---
+++ script/script.js
@@ -0,0 +1,10 @@
+function change()
+    {
+    var elem= document.getElementById("myButton1");
+      if (elem.value=="✕") elem.value= "〇";
+      else elem.value= "✕";
+    }
+ function sample(){
+   document.getElementById("area1").innerText = "変更しました";
+ }
+
